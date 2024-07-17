b18d06f88ef50ee7dc1802f7eec193afc36ada34
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2024-07-17 17:18:11 +0530

    SignUp UI fin

--- frontend/todo/src/pages/SignUp/SignUp.jsx
+++ frontend/todo/src/pages/SignUp/SignUp.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../../components/Navbar/Navbar';
+import PasswordInput from '../../components/Input/PasswordInput';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+
+  const [name , setName] = useState("");
+  const [email , setEmail] = useState("");
+  const [password , setPassword] = useState("");
+  const [error , setError] = useState(null);
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-      SignUp
-    </div>
+    <>
+      <Navbar />
+
+      <div className='flex items-center justify-center mt-28'>
+        <div className='w-96 border rounded bg-white px-7 py-10'>
+          <form onSubmit={handleSignUp}>
+            <h4 className='text-2xl mb-7'>SignUp</h4>
+
+            <input 
+              type='text' 
+              placeholder='Name' 
+              className='input-box' 
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <input 
+              type='text' 
+              placeholder='Email' 
+              className='input-box' 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
+            <button type='submit' className='btn-primary'>
+              Create Account
+            </button>
+
+            <p className='text-sm text-center mt-4'>
+              Already have an account?{" "}
+              <Link to ="/Login" className='font-medium text-primary underline'>
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>      
   )
 }
 
