97cbbac61cbd0debd6f0f2d6a88e870dda0780b5
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2024-07-17 14:15:51 +0530

    React-Router-DOM setup

--- frontend/todo/src/pages/Login/Login.jsx
+++ frontend/todo/src/pages/Login/Login.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import Navbar from "../../components/Navbar/Navbar"
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  return <>
+      <Navbar />
+    </>;
+};
 
 export default Login
