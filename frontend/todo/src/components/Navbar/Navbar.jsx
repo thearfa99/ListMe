c1dee7f099edd01a71f9f0b81e33d0f3fb9ad74b
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2024-07-18 00:35:53 +0530

    Navbar profile

--- frontend/todo/src/components/Navbar/Navbar.jsx
+++ frontend/todo/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import ProfileInfo from '../Cards/ProfileInfo'
+import { useNavigate } from "react-router-dom"
 
 const Navbar = () => {
+
+  const navigate = useNavigate;
+
+  const onLogout = () => {
+    navigate("../Login");
+  };
+
   return (
     <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
         <h2 className='text-xl font-medium text-black py-2'>To-Do</h2>
+
+        <ProfileInfo onLogout={onLogout} />
     </div>
-  )
-}
+  );
+};
 
 export default Navbar
