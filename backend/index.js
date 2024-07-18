b0664b492226aa9eb69fa8786e6a575dfa57708c
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2024-07-18 13:30:52 +0530

    backend midway

---
+++ backend/index.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
+const User = require("./models/user.model");
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
+
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
+app.get("/", (req,res)=>{
+    res.json({ data: "hello" });
+});
+
+//CREATE ACCOUNT
+
+app.post("/create-account", async (req, res) => {
+    const { fullName, email, password } = req.body;
+
+    if (!fullName){
+        return res.status(400).json({ error: true, message: "Full Name is required"});
+    }
+
+    if (!email){
+        return res.status(400).json({ error: true, message: "Email is required"});
+    }
+
+    if (!password){
+        return res.status(400).json({ error: true, message: "Password is required"});
+    }
+
+    const isUser = await User.findOne({ email: email});
+
+    if (isUSer){
+        return res.json({
+            error: true,
+            message: "User already exist",
+        })
+    }
+
+    await user.save();
+
+    const accessToken = jwt.sign({user},process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "3600m",
+    });
+
+    return res.json({
+        error: false,
+        user,
+        accessToken,
+        message: "Registration Successful",
+    })
+});
+
+
+
+app.listen(8000);
+
+module.exports = app;
