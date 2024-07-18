98f9a4ef6519b333a061f6bfc570ecba6c6f9b30
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2024-07-18 16:51:13 +0530

    button colour match

--- frontend/todo/src/components/Todo/Todo.jsx
+++ frontend/todo/src/components/Todo/Todo.jsx
@@ -54,7 +54,7 @@ const Todo = () => {
       {/* INPUT BOX */}
       <div className="flex items-center w-full my-7 bg-gray-200 rounded-full">
         <input ref={inputRef} className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600" type="text" placeholder="Add your task" />
-        <button onClick={add} className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">ADD+</button>
+        <button onClick={add} className="border-none rounded-full bg-primary w-32 h-14 text-white text-lg font-medium cursor-pointer hover:bg-secondary">ADD+</button>
       </div>
 
       {/* TO DO LIST */}
