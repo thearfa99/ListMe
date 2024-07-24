aa8eee77bf8556cb65027a5952778063cec4d5ff
thearfa99 <132257607+thearfa99@users.noreply.github.com>
2024-07-24 13:06:49 +0530

    mhmm

--- frontend/todo/src/App.jsx
+++ frontend/todo/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
-import Home from './pages/Home/Home'
-import Login from './pages/Login/Login'
+import Home from './pages/home/home'
+import Login from './pages/login/login'
 import SignUp from './pages/signup/signup'
 
 const routes = (
